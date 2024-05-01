9ee6b6a81f9b49dbc66f76b737f14c6481007058
Felipe Takeshi <ftrangel07@gmail.com>
2024-04-30 22:09:17 -0300

    cookie no usuario B)

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 const nomes=[]
 const senha=[]
+var cookie_num = 0
 function register(){
+    cookie_num+=1
     username = window.localStorage.setItem("user_name",document.getElementById("user").value)
     nomes.push(window.localStorage.getItem("user_name"))
     password = window.localStorage.setItem("user_pass",document.getElementById("pass").value)
     senha.push(window.localStorage.getItem("user_pass"))
     alert(nomes)
     alert(senha)
+    document.cookie = document.getElementById("user").value + "=" + cookie_num + ";path/"
 }
 function login(){
     var nome_true = Boolean(false)
     var senha_true = Boolean(false)
     
-    for (let i = 0; i < nomes.length; i++) {
+    for (var i = 0; i < nomes.length; i++) {
         if(nomes[i] == document.getElementById("user_reg").value){
             nome_true=true
         }
     }
-    for (let e = 0; e < senha.length; e++) {
-        if(senha[e] == document.getElementById("pass_reg").value){
-            senha_true=true         
-        }
+    if(senha[i] == document.getElementById("pass_reg").value){
+        senha_true=true
     }
     if(nome_true==true && senha_true==true){
         alert("VOCÊ ESTÁ LOGADO!")
